cb4fe8fb6995af5cf06bfe0fe9e7d112e918cf89
BriceCoop <bricecooper2004@gmail.com>
2024-11-16 11:10:19 -0500

    World with destructible resources

---
+++ new-game-project/Scene/Rock.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@export var health:int = 25
+
+
+func _on_area_entered(area: Area2D) -> void:
+	#if area.name.contains("resource tool"):
+	health -= 20  #area.dmg
+	CheckHealth()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("K"):
+		_on_area_entered(null)
+		print(health)
+		
+
+func CheckHealth():
+	if health <= 0:
+		queue_free()
