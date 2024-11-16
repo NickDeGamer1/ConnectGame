66324acb309d6a7f077f93d7db1f4b203ed5899c
BriceCoop <bricecooper2004@gmail.com>
2024-11-16 15:11:03 -0500

    Add 'day and night'

--- new-game-project/Scene/day_and_night_cycle.gd
+++ new-game-project/Scene/day_and_night_cycle.gd
@@ -1,1 +1,16 @@
 extends CanvasModulate
+
+
+func _ready() -> void:
+	DayCycle.period_changed.connect(whatever)
+
+
+func whatever(period):
+	match period:
+		DayCycle.TimeOfDay.DAY:
+			pass
+		DayCycle.TimeOfDay.DUSK:
+			pass
+		_:
+			pass
+	pass
