f8b567317dafb4502841265123e4741468c54e35
BriceCoop <bricecooper2004@gmail.com>
2024-11-16 15:23:04 -0500

    Restore on Daytime

--- new-game-project/Prop.gd
+++ new-game-project/Prop.gd
@@ -11,9 +11,20 @@ func CheckHealth():
 
 func _ready() -> void:
 	health = max_health
+	DayCycle.period_changed.connect(_on_period_changed)
+
 
 func addResource():
 	print("Prop.gd has no resource! overwrite me!")
+
+func _on_period_changed(period):
+	match period:
+		DayCycle.TimeOfDay.DAY:
+			restore()
+		_:
+			pass
+	pass
+
 
 func restore():
 	visible = true
