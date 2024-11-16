ad751f4ba20ec134337d893b7e0d30cfdea5037b
EagleEyeScouter <dhopkephotos@gmail.com>
2024-11-16 12:30:36 -0500

    Add NPC logic (not working!)

--- new-game-project/npc.gd
+++ new-game-project/npc.gd
@@ -1,10 +1,13 @@
-extends Node2D
+extends CharacterBody2D
 
-var target : Node2D
-var 
+@export var target : Node2D
+var nav_agent : NavigationAgent2D
+@export var max_velocity := 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	nav_agent = NavigationAgent2D.new()
+	add_child(nav_agent)
 	pass # Replace with function body.
 
 
@@ -13,12 +16,15 @@ func _process(delta: float) -> void:
 	pass
 
 
-func retarget():
-	pass
+func retarget(thing:Node2D=target):
+	target = thing
+	if nav_agent != null:
+		nav_agent.target_position = target.position
 
-
-func approach_target():
+func _physics_process(delta):
 	if target == null or self.get_tree() != target.get_tree():
 		return
 	
-	
+	var direction = nav_agent.get_next_path_position()
+	self.velocity = direction.normalized() * max_velocity
+	move_and_slide()
