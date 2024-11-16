cc136b2a5f5419cd62adddee1edf276636a1c501
BriceCoop <bricecooper2004@gmail.com>
2024-11-16 18:35:48 -0500

    Walls and Wall placement

--- new-game-project/Scene/wall_health.gd
+++ new-game-project/Scene/wall_health.gd
@@ -1,11 +1,12 @@
 extends Area2D
-@export var health:int = 50
+@export var max_health:int = 50
+@export var health := 50
 
 
 func _on_area_entered(area: Area2D) -> void:
-	#if area.name.contains("monster"):
-	health -= 20  #area.dmg
-	CheckHealth()
+	if area.name.contains("enemy"):
+		health -= 20  #area.dmg
+		CheckHealth()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("the j"):
@@ -16,3 +17,11 @@ func _input(event: InputEvent) -> void:
 func CheckHealth():
 	if health <= 0:
 		queue_free()
+		
+func restore():
+	visible = true
+	health = max_health
+
+func build_Wall():
+	if PlayerShared.ResourceWood == 3 or PlayerShared.ResourceStone == 3:
+		restore()
