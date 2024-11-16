48be7074b97bef8a31408b17c8aa3b0e48002141
EagleEyeScouter <dhopkephotos@gmail.com>
2024-11-16 11:04:51 -0500

    Update player_data.gd

--- new-game-project/player/player_data.gd
+++ new-game-project/player/player_data.gd
@@ -7,16 +7,20 @@ signal recovered_health(heal:float)
 signal took_damage(damage:float)
 
 """
-	Use this function for all healing/damage applied to players.
-	This will be redirected to the relevant function 
+	Use this function for healing/damage applied to players.
+	This will be redirected to the relevant function
+	Those can also be used, but like...
 """
 func apply_health_change( delta:float ):
-	if delta < 0:
+	
+	## If delta is 0, there's no point in sending a signal.
+	if is_zero_approx(delta):
+		return
+	## If delta is negative, take damage.
+	elif delta < 0:
 		_apply_damage( abs(delta) )
 	else:
 		_apply_healing( delta )
-	
-	pass
 
 
 func _apply_damage( damage:float ):
@@ -26,4 +30,4 @@ func _apply_damage( damage:float ):
 
 func _apply_healing( heal:float ):
 	health += heal
-	pass
+	recovered_health.emit(heal)
