7471bd7f0f5679213b3a7c7cac726c0720b9fdff
EagleEyeScouter <dhopkephotos@gmail.com>
2024-11-16 20:00:05 -0500

    Start adding input processing

--- new-game-project/weapon_select_menu.gd
+++ new-game-project/weapon_select_menu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var player_index := 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +9,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_down"):
+		pass
+	pass
+
+
+func _churn_and_continue():
+	
+	
+	player_index += 1
+	if player_index > Player.PNum.size():
+		_move_on()
+	pass
+
+
+## TODO: needs to do something!
+func _move_on():
 	pass
